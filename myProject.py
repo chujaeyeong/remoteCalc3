4c8012fc3e325b61904bb84925e165d4fd018601
jae yeong <chujy1224@gmail.com>
2023-02-28 10:45:50 +0900

    subCalc Commit 3

--- myProject.py
+++ myProject.py
@@ -7,16 +7,22 @@ def min_func(n1, n2) :
     res = n1 - n2
     return res
 
+def mul_func(n1, n2) :
+    res = n1 * n2
+    return res
+
 
 ## 전역 변수 (인스턴트 변수, 클래스 변수)
 num1, num2, result = 100, 200, 0
 
 
 ## 메인 코드부 (main() 메서드)
-result1 = add_func(num1, num2)
 
+result1 = add_func(num1, num2)
 print(num1, '+', num2, '=', result1)
 
 result2 = min_func(num1, num2)
-
 print(num1, '-', num2, '=', result2)
+
+result3 = mul_func(num1, num2)
+print(num1, '8', num2, '=', result3)
