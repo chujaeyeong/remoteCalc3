15c5004cd34f3d351fc8a10b4870bb2a0c636d5f
jae yeong <chujy1224@gmail.com>
2023-02-28 10:43:06 +0900

    MasterCommit 2

--- myProject.py
+++ myProject.py
@@ -1,7 +1,11 @@
 ## 메서드 선언부
 def add_func(n1, n2) :
-    res1 = n1 + n2
-    return res1
+    res = n1 + n2
+    return res
+
+def min_func(n1, n2) :
+    res = n1 - n2
+    return res
 
 
 ## 전역 변수 (인스턴트 변수, 클래스 변수)
@@ -12,3 +16,7 @@ num1, num2, result = 100, 200, 0
 result1 = add_func(num1, num2)
 
 print(num1, '+', num2, '=', result1)
+
+result2 = min_func(num1, num2)
+
+print(num1, '-', num2, '=', result2)
